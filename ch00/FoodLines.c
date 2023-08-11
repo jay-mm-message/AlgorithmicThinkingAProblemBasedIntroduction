ddd6568dc83b382d639b82d623ef224d373002cb
jay <jay.mm.message@gmail.com>
2023-08-12 00:22:54 +0800

    FoodLines refactor

--- ch00/FoodLines.c
+++ ch00/FoodLines.c
@@ -5,73 +5,51 @@
 
 #define MAX_LINES 100
 
-void join_people(int ptr[], int ptr2[], int n, int m)
+int short_line(int line[], int n) 
 {
-    int ptr2_pos = 0;
-    while ( m > 0 )
-    {
-        /* code */
-        int min_index = -1;
-        int min_value = ptr[0];
-        bool find_min = false;
-        int count = n;
-
-        for (size_t i = 0; i < count; i++)
-        {
-            /* code */
-            if (min_value > ptr[i]) {
-                min_value = ptr[i];
-                min_index = i;
-                find_min = true;
-            }
-
-        }
-
-        if (!find_min) {
-            min_index = 0;
-            find_min = false;
-        }
-        ptr2[ptr2_pos] = ptr[min_index];
-        ptr2_pos = ptr2_pos + 1;
-        ptr[min_index] = ptr[min_index] + 1;
-
-        m = m - 1;
-    } // while
-
-}
-
-void show_result(int ptr2[], int join_m) 
-{
-    int count = join_m;
+    int min = 0;
+    int count = n;
     for (size_t i = 0; i < count; i++)
     {
         /* code */
-        int result = ptr2[i];
-        printf("%d\n", result);
+        if (line[i] < line[min])
+        {
+            /* code */
+            min = i;
+        } 
     }
-    printf("\n");
+    return min;
+}
+
+void solve(int line[], int n)
+{
+    int short_loc = short_line(line, n);
+    int short_value = line[short_loc];
+    printf("%d\n", short_value);
+    
+    line[short_loc] = line[short_loc] + 1;
 }
 
 int main(void) {
     int n = 0, m = 0;
     scanf("%d %d", &n, &m);
   
-    int ptr[MAX_LINES];
-    int ptr2[MAX_LINES];
+    int line[MAX_LINES];
 
     int count = n;
     for (size_t i = 0; i < count; i++)
     {
         /* code */
-        scanf("%d", ptr + i);
+        scanf("%d", &line[i]);
+    }
+    
+    count = m;
+    for (size_t i = 0; i < count; i++)
+    {
+        /* code */
+        solve(line, n);
     }
 
-//void join_people(int ptr[], int ptr2[], int n, int m)
-    join_people(ptr, ptr2, n, m);
-//void show_result(int ptr2[], int join_m)
-    show_result(ptr2, m);
-
     printf("\n");
-
     return 0;
 }
